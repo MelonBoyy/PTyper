65f129c2ff2db85755a9f0c112627413c4001562
James Sheppard <jameslawrencesheppard@gmail.com>
2026-04-09 01:06:53 -0500

    Whoops, forgot the driver in the production build needs to use the release .dlls to test.

--- production-build-linux.sh
+++ production-build-linux.sh
@@ -4,5 +4,6 @@ echo "BUILDING PTYPER LINUX"
 
 g++ -O3 -shared -fPIC -o "builds/release/PTyper.dll" "src/ptyper.cpp"
 g++ -O3 -shared -fPIC -o "builds/release/PTyper-Threaded.dll" "src/ptyper-threaded.cpp"
+g++ -g "src/driver.cpp" -o "builds/release/driver" "builds/release/PTyper.dll" "builds/release/PTyper-Threaded.dll"
 
 echo "FINISHED BUILDING PTYPER LINUX"
